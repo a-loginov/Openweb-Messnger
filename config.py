0280775a0bba4f8f1eccc6276200c5bfe8fa1802
Alexey Loginov <a-loginov@school1409.ru>
2026-08-13 21:07:02 +0300

    fix

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Настройки Flask приложения
-SECRET_KEY = os.environ("SECRET_KEY")
-ADMIN_USERNAME = os.environ("ADMIN_USERNAME")
-ADMIN_PASSWORD = os.environ("ADMIN_PASSWORD")
+SECRET_KEY = os.environ["SECRET_KEY"]
+ADMIN_USERNAME = os.environ["ADMIN_USERNAME"]
+ADMIN_PASSWORD = os.environ["ADMIN_PASSWORD"]
 
 
